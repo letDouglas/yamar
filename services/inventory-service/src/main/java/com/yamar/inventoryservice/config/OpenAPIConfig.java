dd21e9b6a1506b494cbbbb8d34fb44f36302b59d
letDouglas <borsettidamirdouglas@gmail.com>
2025-06-07 13:58:55 +0200

    feat(docs/inventory): Add OpenAPI support to inventory-service
    - Added springdoc-openapi dependency to inventory-service pom.xml for API documentation.
    - Created OpenAPIConfig class to configure Swagger/OpenAPI endpoints and servers.
    - Updated application.yml to set swagger-ui and api-docs paths.

---
+++ services/inventory-service/src/main/java/com/yamar/inventoryservice/config/OpenAPIConfig.java
@@ -0,0 +1,64 @@
+package com.yamar.inventoryservice.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import jakarta.servlet.ServletContext;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Value("${server.port}")
+    private int port;
+
+    @Bean
+    public OpenAPI customOpenAPI(ServletContext servletContext) {
+        return new OpenAPI()
+                .info(apiInfo())
+                .externalDocs(externalDocs())
+                .servers(apiServers(servletContext));
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Inventory Service API")
+                .description("RESTful microservice to manage inventory in the catalog")
+                .version("1.0.0")
+                .license(new License()
+                        .name("Apache License 2.0")
+                        .url("https://www.apache.org/licenses/LICENSE-2.0"))
+                .contact(new Contact()
+                        .name("Derain Dev Team")
+                        .email("xmor@gmail.com"));
+    }
+
+    private ExternalDocumentation externalDocs() {
+        return new ExternalDocumentation()
+                .description("Inventory Service Documentation")
+                .url("https://xmor.com/docs/inventory-service");
+    }
+
+    private List<Server> apiServers(ServletContext servletContext) {
+        String contextPath = servletContext.getContextPath(); // es. /api/v1
+        String localUrl = String.format("http://localhost:%d%s", port, contextPath);
+        String prodUrl = "https://xmor.com/inventory-service";
+
+        Server localServer = new Server()
+                .url(localUrl)
+                .description("Local Dev Server");
+
+        Server prodServer = new Server()
+                .url(prodUrl)
+                .description("Production Server");
+
+        return List.of(localServer, prodServer);
+    }
+}
